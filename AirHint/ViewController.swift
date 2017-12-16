b9b4c375b36f3f3f643add3a7af3deb7bc4fa22a
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2017-12-16 17:36:24 +0100

    Initial Commit

---
+++ AirHint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirHint
+//
+//  Created by Andrii Kuzminskyi on 16/12/2017.
+//  Copyright © 2017 A.Kuzminskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
